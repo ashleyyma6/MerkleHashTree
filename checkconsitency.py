7a7f098694ee9766ace2882d748665bd3412bf2c
Ashley Ma <euyam458689881@gmail.com>
2022-03-13 17:03:02 -0700

    add consistency check comment

---
+++ checkconsitency.py
@@ -0,0 +1,3 @@
+# old merkle tree hash is a subset of the new merkle tree hash
+# the new merkle tree hash is the concatenation of the old merkle tree hash 
+# plus all the intermediate node hashes of the newly appended strings
