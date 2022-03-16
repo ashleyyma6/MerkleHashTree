ae15f94aa63244175a092d376f87245be91623f5
Ashley Ma <euyam458689881@gmail.com>
2022-03-15 21:41:31 -0700

    finish build data structure

--- buildmtree.py
+++ buildmtree.py
@@ -37,18 +37,20 @@ class Node:
     def add_parent(self, parent):
         self.parent = parent
     
-    def update_hash(self):
+    def update_hash(self, right=None):
         if self.data:
-            # print("leaf")
+            print("leaf: ",self.data)
             self.hashv = get_node_hash(self.data)
         else:
             print("node")
             left_h=""
             right_h=""
-            if self.left:
+            if self.left.hashv:
                 left_h = self.left.hashv
-            if self.right:
+            if self.right.hashv:
                 right_h = self.right.hashv
+            else:
+                right_h = right
             self.hashv = get_node_hash(left_h+right_h)
         
 class MerkleTree:
@@ -56,12 +58,15 @@ class MerkleTree:
         self.root = None
         self.tree = []
         self.leafNodes = []
+        print("creat tree")
     
     def add_leaf(self, data_strings):
         for data in data_strings:
             n = Node(data,None,None,None,None)
             n.update_hash()
             self.leafNodes.append(n)
+        print("add leaf: ",len(self.leafNodes))
+        self.printLeaf()
 
     def get_tree_level(self):
         return get_largest_power_2(len(self.leafNodes))
@@ -69,11 +74,9 @@ class MerkleTree:
     def build_tree(self):
         tree = []
         tree.append(self.leafNodes) #add leaf node hashes to tree
-        # print("===leaf_node_hashes===")
-        # print(leaf_node_hashes)
         curr_nodes = self.leafNodes
+        print("curr_nodes loaded")
         while(len(curr_nodes)!=1):
-            #print(curr_hashes)
             node_index = 0
             new_upper_nodes = []
             while(node_index<len(curr_nodes)):
@@ -86,7 +89,7 @@ class MerkleTree:
                     right_child = curr_nodes[node_index]  
                 # set up parent, link to child         
                 if(right_child == None):
-                    # no right child
+                    # no right sibling child
                     print("no right child")
                     # Empty intermediate node, no hash should be update
                     # only for link to the left child at lower level
@@ -94,24 +97,25 @@ class MerkleTree:
                     left_child.parent = parent
                     new_upper_nodes.append(parent)
                 else:
-                    # right_child is not none
+                    # right sibling child is not none
                     # if right child is a Empty intermediate node, need to reach to lower level to get hash
-                    while(right_child.hashv==None):
-                        right_child = right_child.left
                     # new parent
-                    print("left_hash: ",left_child.hashv)
-                    print("right_hash: ",right_child.hashv)
                     parent = Node(None,left_child,right_child,None,None)
-                    parent.update_hash()
-                    print("new_hash: ",parent.hashv)
                     # add parent to child
                     left_child.parent = parent
-                    right_child.parent = parent                
+                    right_child.parent = parent 
+                    # keep empty intermediate node in the tree
+                    # while -> find the real right child hash
+                    while(right_child.hashv==None):
+                        print("right sibling child is none")
+                        right_child = right_child.left
+                    print("left_hash: ",left_child.hashv[0:5])
+                    print("right_hash: ",right_child.hashv[0:5])
+                    parent.update_hash(right_child.hashv)
+                    print("parent hash: ",parent.hashv[0:5])          
                     new_upper_nodes.append(parent)    
                 node_index+=1
-            # print(len(new_hashes))
-            # print("===new_hashes===")
-            # print(new_hashes)        
+            print("add upper level nodes: ",len(new_upper_nodes))      
             tree.insert(0,new_upper_nodes)
             curr_nodes=new_upper_nodes
         return tree
@@ -121,6 +125,9 @@ class MerkleTree:
             print("=====")
             for node in level:
                 print(node.hashv)
+    def printLeaf(self):
+        for node in self.leafNodes:
+            print(node.hashv[0:5])
 
 # def insert_node(tree, data_string):
 #     if(len(tree)==0):
@@ -128,12 +135,11 @@ class MerkleTree:
 #         tree.append(leaf_level)
 
 # ./buildmtree.py [alice,bob,carlol,david]
-# no space
+# ./buildmtree.py [0,1,2,3,4,5,6,7]
 def load_input(input_string):
     input = input_string[1:-1]
     input_list = input.split(',')
-    #print(input_list)
-    # print("load input")
+    print("load input: ",input_list)
     return input_list
 
 # def export_tree(tree):
@@ -146,12 +152,11 @@ def load_input(input_string):
 def main():
     input_list = test_data
     if(len(sys.argv)>1):
-        #print(sys.argv[1])
         input_list = load_input(sys.argv[1])
     merkle_hash_tree = MerkleTree()
     merkle_hash_tree.add_leaf(input_list)
     merkle_hash_tree.build_tree()
-    merkle_hash_tree.printTree()
+    # merkle_hash_tree.printTree()
     # print("===merkle_hash_tree===")
     # print(merkle_hash_tree)
     # export_tree(merkle_hash_tree)
